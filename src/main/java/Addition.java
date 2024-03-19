812238378240180df32236e9f8f819f566f16d70
Pranathi <pranathiichennupati@gmail.com>
2024-03-19 15:05:19 +0530

    Submitted the code

---
+++ src/main/java/Addition.java
@@ -0,0 +1,14 @@
+public class Addition{
+	public void add(){
+		short a=1;
+		short b=2;
+		short c=3;
+		short d=4;
+		short e=5;
+		short f=6;
+		int res=a+b+c+d+e+f;
+		System.out.println(res);
+}
+}
+		
+		
